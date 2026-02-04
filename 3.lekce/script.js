4f829304313d454cc839bf73f57a016c02de58ba
ondra hlady <hladky.ondrej@centrum.cz>
2026-02-04 11:32:52 +0100

    lekce3

---
+++ 3.lekce/script.js
@@ -0,0 +1,20 @@
+let userName="metr pacinka"
+console.log(userName)
+
+userName="goy"
+console.log(userName)
+
+console.log(6+7)
+console.log("6"+7)
+console.log("6"+"7")
+
+let age=41;
+console.log(age>20);
+
+let text="text"
+console.log(typeof text)
+
+let a;
+console.log(a);
+let b=null;
+console.log(b);

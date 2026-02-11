7b26fb652d21d3e9c8c72efbd7a76953aac40116
ondra hlady <hladky.ondrej@centrum.cz>
2026-02-11 11:52:36 +0100

    .

--- 3.lekce/script.js
+++ 3.lekce/script.js
@@ -18,3 +18,32 @@ let a;
 console.log(a);
 let b=null;
 console.log(b);
+
+let isloggedin = false
+console.log(typeof isloggedin)
+
+//aritmetika
+
+let c=3
+let d=2
+console.log(c+d);
+console.log(c-d);
+console.log(c*d);
+console.log(c/d);
+console.log(c**d); // mocnění
+console.log(c%d); //zbytek
+console.log(c + d * d);
+console.log((c+d)*d);
+
+//aktualizace proměné -přiřazování
+
+let number=20;
+number=number+2;
+number+=2; //toto je stený zápis jako o řádek víš
+number-=2;
+number/=2;
+number*=2;
+number%=2;
+number++;
+number--;
+console.log(number)
---
+++ cvičení/script.js
@@ -0,0 +1,8 @@
+let jm1="pepy";
+let jm2="franty";
+let age1=20;
+let age2=25;
+let součet = age1+age2;
+let text="text"
+console.log("součet věků " + jm1 + " a " + jm2 + " je " + součet);
+ 

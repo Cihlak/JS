bef8b83ff7ca1231395e899415e637f823f13e28
ondra hlady <hladky.ondrej@centrum.cz>
2026-03-25 11:34:58 +0100

    .

---
+++ cvičení/script5-cviceni.js
@@ -0,0 +1,17 @@
+let podmínka = !false;
+podmínka = true && false;
+podmínka = true || false
+console,log(podmínka)
+
+let number = 10;
+let mssage;
+if (number > 0) {
+    message = "číslo je kladné"
+} else {
+    message = "číslo není kladné"
+}
+console.log(message)
+
+let number2 = 10;
+let message2 = (numer2 > 0) ? "kladné číslo" : "nekladné číslo";
+console.log(message2)

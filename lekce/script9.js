8476505ffae212722616202014d409eafe4a2c08
ondra hlady <hladky.ondrej@centrum.cz>
2026-05-20 11:31:27 +0200

    .

---
+++ lekce/script9.js
@@ -0,0 +1,22 @@
+
+console.log("it's anice day")
+console.log('moje jméno je "Ondra"')
+
+let username = "tonda"
+let message = `Moje jméno je ${username}`;
+console.log(message);
+
+let a = 5;
+let b = 10;
+let součet = `Součet je ${a + b}`
+console.log(součet)
+
+function pozdrav(name) {
+    return `hello, ${name}`;
+}
+console.log(pozdrav("honza"))
+
+let popis = `toto je dlouhý 
+text který se rozprostírá 
+přes několik řádků`
+console.log(popis)
